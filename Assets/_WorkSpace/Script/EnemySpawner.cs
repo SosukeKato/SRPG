b4a016f21c170e472d0926459b525f5e6cb5467f
SosukeKato <dokindokinpiano@gmail.com>
2025-06-24 14:17:09 +0900

    [Update]EnemySpawner.csにランダムなEnemyをランダムに選択されたEnemySpawnerからSpawnする処理を追加(エラーあり)

--- Assets/_WorkSpace/Script/EnemySpawner.cs
+++ Assets/_WorkSpace/Script/EnemySpawner.cs
@@ -8,9 +8,16 @@ public class EnemySpawner : MonoBehaviour
     List<GameObject> _EnemyList;
     [SerializeField]
     List<Transform> _EnemySpawnerList;
+    [SerializeField]
+    int _EnemySpawnNumber = 0;
+
+    int r;
+    GameObject obj;
     // Start is called before the first frame update
     void Start()
     {
+        r = Random.Range(0, _EnemyList.Count);
+        obj = Instantiate(_EnemyList, _EnemySpawnerList[r].position,Quaternion.identity);   
         
     }
 
