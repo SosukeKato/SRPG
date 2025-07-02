8196735bb1a7aceb5e407cb580460908420f38ad
SosukeKato <dokindokinpiano@gmail.com>
2025-07-02 18:08:28 +0900

    [fix]Editor上でステージの大きさを指定できるように変更

--- Assets/_WorkSpace/Script/Stage/RandomStageCreate.cs
+++ Assets/_WorkSpace/Script/Stage/RandomStageCreate.cs
@@ -6,14 +6,18 @@ public class RandomStageCreate : MonoBehaviour
 {
     [SerializeField]
     List<GameObject> _FieldBlock;
+    [SerializeField]
+    int x = 15;
+    [SerializeField]
+    int z = 15;
 
     int r;
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < 15; i++) 
+        for (int i = 0; i < x; i++) 
         {
-            for (int j = 0; j < 15; j++)
+            for (int j = 0; j < z; j++)
             {
                 r = Random.Range(0, _FieldBlock.Count);
                 Instantiate(_FieldBlock[r], new Vector3(i, 0, j), Quaternion.identity);
