aa1635e6151ccb8913f77616e5ef54910db81d7f
SosukeKato <dokindokinpiano@gmail.com>
2025-07-02 18:15:36 +0900

    [fix]RandomStageCreate.cs内の変数x,zを変数_StageX,_StageZに改名

--- Assets/_WorkSpace/Script/Stage/RandomStageCreate.cs
+++ Assets/_WorkSpace/Script/Stage/RandomStageCreate.cs
@@ -7,17 +7,17 @@ public class RandomStageCreate : MonoBehaviour
     [SerializeField]
     List<GameObject> _FieldBlock;
     [SerializeField]
-    int x = 15;
+    int _StageX = 15;
     [SerializeField]
-    int z = 15;
+    int _StageZ = 15;
 
     int r;
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < x; i++) 
+        for (int i = 0; i < _StageX; i++) 
         {
-            for (int j = 0; j < z; j++)
+            for (int j = 0; j < _StageZ; j++)
             {
                 r = Random.Range(0, _FieldBlock.Count);
                 Instantiate(_FieldBlock[r], new Vector3(i, 0, j), Quaternion.identity);
